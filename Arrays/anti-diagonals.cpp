f3575a41609f1de43e180b2b5b2822aaee06c8db
Ahmed Atito <30963134+atitoa93@users.noreply.github.com>
2018-09-29 16:36:41 -0500

    simulation

---
+++ Arrays/anti-diagonals.cpp
@@ -0,0 +1,13 @@
+// https://www.interviewbit.com/problems/anti-diagonals/
+
+vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
+  vector <vector <int> > ret(2 * A.size() - 1);
+  
+  for (int i = 0; i < A.size(); ++i) {
+    for (int j = 0; j < A[i].size(); ++j) {
+      ret[i + j].push_back(A[i][j]);
+    }
+  }
+  
+  return ret;
+}

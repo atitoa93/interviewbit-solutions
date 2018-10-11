c6fe9f200ced5944751d122f67913439acff95f1
Ahmed Atito <30963134+atitoa93@users.noreply.github.com>
2018-10-11 12:38:22 -0500

    base conversion

---
+++ Math/excel-column-number.cpp
@@ -0,0 +1,9 @@
+// https://www.interviewbit.com/problems/excel-column-number/
+
+int Solution::titleToNumber(string A) {
+  int ret = 0;
+  for (int i = 0; i < A.size(); ++i) {
+    ret = 26 * ret + (A[i] - 'A' + 1);
+  }
+  return ret;
+}

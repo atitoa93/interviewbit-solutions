9faae1b44a1f2e783d2b83810445e4ddbec847b1
Ahmed Atito <30963134+atitoa93@users.noreply.github.com>
2019-05-11 23:53:08 -0500

    greedy

--- Dynamic Programming/min-jumps-array.cpp
+++ Dynamic Programming/min-jumps-array.cpp
@@ -1,23 +1,20 @@
 // https://www.interviewbit.com/problems/min-jumps-array/
 
 int Solution::jump(vector<int> &A) {
-  int ret = 0;
-  
-  for (int i = A.size() - 1; i > 0; --i) {
-    int farthest = -1;
-    for (int j = i - 1; j >= 0; --j) {
-      if (A[j] + j >= i) {
-        farthest = j;
-      }
-    }
-    if (~farthest) {
-      i = farthest + 1;
-      ret += 1;
-    } else {
-      ret = -1;
-      break;
+  int n = A.size();
+  if (n <= 1) return 0;
+
+  int ret = 1;
+  int reachablePos = A[0];
+  int maxReachablePos = A[0];
+  for (int i = 1; i <= reachablePos; ++i) {
+    if (i == n - 1) return ret;
+    maxReachablePos = max(maxReachablePos, A[i] + i);
+    if (i == reachablePos) {
+      reachablePos = maxReachablePos;
+      ++ret;
     }
   }
   
-  return ret;
+  return -1;
 }
